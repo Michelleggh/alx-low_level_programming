11e3cfaa47206a93f5e34dad8fb4e9d6432f1461
Michelleggh <muhoromichelle@gmail>
2022-04-20 14:56:17 -0700

    Task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 void reset_to_98(int *n);
+int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -8,7 +10,10 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print-array(int *a, int n);
+void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _putchar(char c);
+int _atoi(char *s);
+
+#endif
+
 
