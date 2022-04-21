b2abcc09bf2a28680904360b76ed70efe506af27
Michelleggh <muhoromichelle@gmail>
2022-04-21 01:17:42 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,7 +5,7 @@
  * @s: string to convert
  * Description: converts string to int considering all negatives
  * Return: integer
-*/ 
+*/
 
 int _atoi(char *s)
 {
@@ -14,7 +14,7 @@ int i, val, sign;
 val = 0;
 sign = 1;
 
-for (i = 0; s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'); i++)
+or (i = 0; s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'); i++)
 {
 if (s[i] == '-')
 sign = sign * -1;
@@ -29,4 +29,3 @@ return (val);
 }
 return (val);
 }
-
