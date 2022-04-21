cc5bfcc8ca20073addddbe84df024e7afa44c90b
Michelleggh <muhoromichelle@gmail>
2022-04-21 01:50:54 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,10 +14,10 @@ int sign = 1;
 unsigned int num = 0;
 
 do {
-if (*s=='-')
+if (*s == '-')
 sign *= 1;
 
-else if (*s>='0' && *s <= '9')
+else if (*s >= '0' && *s <= '9')
 num = (num * 10) + (*s - '0');
 
 else if (num > 0)
