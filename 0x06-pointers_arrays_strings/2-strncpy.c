d0f12b9b83484d97d5d98d26a887e823711351f7
Michelleggh <muhoromichelle@gmail>
2022-04-24 14:23:36 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 int i;
 
 for (i = 0; i < n && src[i] != '\0'; i++)
-{					
+{
 dest[i] = src[i];
 }
 
@@ -23,7 +23,5 @@ while (i < n)
 dest[i] = '\0';
 i++;
 }
-
 return (dest);
 }
-
