8b741cee357840e30f7a0e198f42287981c5448b
Michelleggh <muhoromichelle@gmail>
2022-04-24 14:32:44 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates specified values from src to dest
+ * @src: second string to copy from
+ * @dest: string to be overwritten
+ * @n: number of values to concatenate
+ * Description: concatenates n number of values from src to end of dest
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+
+i = 0;
+j = 0;
+
+while (dest[i] != '\0')
+i++;
+
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;														}
+
+dest[i] = '\0';
+return (dest);
+}
