55a8635417a0199df77d99e519bec725245ada8c
Michelleggh <muhoromichelle@gmail>
2022-04-26 16:17:27 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,12 +19,13 @@ x = 0;
 if (haystack[x] == needle[x])
 {
 do {
-																		if (needle[x + 1] == '\0')
-																		return (haystack);
+
+if (needle[x + 1] == '\0')
+return (haystack);
 
 x++;
-																		} while (haystack[x] == needle[x]);
-																		}
+} while (haystack[x] == needle[x]);					
+}
 
 haystack++;
 }
