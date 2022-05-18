c99e5aae6f1922fc62ae1f1c770822caec17445d
Michelleggh <muhoromichelle@gmail>
2022-05-18 00:27:54 -0700

    Tasks

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+/**
+ *op_add - Add two integers
+ *
+ *@a: integer 1
+ *@b: integer 2
+ *
+ *Return: Integer
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ *op_sub - Sub two intergers
+ *
+ *@a: integer 1
+ *@b:integer 2
+ *
+ *Return: Integer 
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - Product of two integers
+ *
+ *@a: integer 1
+ *@b: integer 2
+ *
+ *Return: Integer
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ *op_div - Result of the dividion os two integers
+ *
+ *@a: integer 1
+ *@b: integer 2
+ *
+ *Return: Integer
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ *op_mod - Remainder of the division
+ *
+ *@a: integer 1
+ *@b: integer 2
+ *
+ *Return: Integer
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
